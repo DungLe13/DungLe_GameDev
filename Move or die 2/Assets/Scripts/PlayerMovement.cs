741f8168f7c7946a72cf1d81cffab67066a86257
dorynguyen <knguyen@Kys-MacBook-Pro.local>
2016-04-19 01:38:03 -0400

    Character moves(no jump)
    Scene is basically set up and the character is moving left to right

---
+++ Move or die 2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 10f;
+	private Rigidbody2D playerRigid;
+
+	void Start () {
+	
+	}
+	
+	void FixedUpdate () {
+		float movement = Input.GetAxis ("Horizontal");
+		playerRigid = GetComponent<Rigidbody2D>();
+		playerRigid.velocity = new Vector2 (movement * speed, playerRigid.velocity.y);
+
+		//GetComponent<Rigidbody2D>().velocity = new Vector2 (movement * speed, rigidbody2D.velocity.y);
+
+	}
+}
