e9ebba07f0e222c9f735766366e500a90832a0a6
dorynguyen <knguyen@Kys-MacBook-Pro.local>
2016-04-20 23:07:58 -0400

    Jumping almost done
    Still have issue with response rate when pressing spacebar

--- Move or die 2/Assets/Scripts/PlayerMovement.cs
+++ Move or die 2/Assets/Scripts/PlayerMovement.cs
@@ -1,34 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
 	public float speed = 10f;
-	public float jumpHeight=20f;
+	public float jumpHeight = 20f;
+	float previousY;
 	private Rigidbody2D player1Rigid;
-	void Start () {
+	bool letsJump;
+
+	void Start ()
+	{
 	
 	}
 
-	void Update(){
-		if (Input.GetKeyDown ("space")) {
-			player1Rigid.velocity = new Vector2 (player1Rigid.velocity.x, player1Rigid.position.y+jumpHeight);
+
+
+
+	
+	void FixedUpdate ()
+	{
+		float currentY = GetComponent<Rigidbody2D> ().position.y;
+		if (currentY != previousY) {
+			letsJump = false;
+			Debug.Log ("false");
+
+		} else {
+			letsJump = true;
+			Debug.Log ("true");
 
 		}
-		
-	
-	
-	
-	}
 
-	
-	void FixedUpdate () {
+		previousY=currentY;
 		float horizontal_movement = Input.GetAxis ("Horizontal");
-		//float vertical_movement = Input.GetAxis ("Jump");
-		player1Rigid = GetComponent<Rigidbody2D>();
+		player1Rigid = GetComponent<Rigidbody2D> ();
 		player1Rigid.velocity = new Vector2 (horizontal_movement * speed, player1Rigid.velocity.y);
-		//player1Rigid.velocity = new Vector2 (player1Rigid.velocity.x, vertical_movement * speed);
 
+		if ((Input.GetKeyDown ("space"))&&(letsJump==true)) {
+			player1Rigid.velocity = new Vector2 (player1Rigid.velocity.x, player1Rigid.position.y + jumpHeight);
+
+		}
 
 	}
 }
