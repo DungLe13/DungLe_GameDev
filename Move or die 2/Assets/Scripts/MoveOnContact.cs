b2adbd450e15595ef63268585b0fb67b93327409
dorynguyen <knguyen@Kys-MacBook-Pro.local>
2016-04-27 12:52:52 -0400

    Detect collision
    Havent figured out how to transform yet

--- Move or die 2/Assets/Scripts/MoveOnContact.cs
+++ Move or die 2/Assets/Scripts/MoveOnContact.cs
@@ -3,7 +3,14 @@ using System.Collections;
 
 public class MoveOnContact : MonoBehaviour {
 
-	void OnTriggerEnter2D(Collider2D other){
-		transform.Translate (Vector2.up);
+	void OnCollisionEnter2D(Collision2D Player) {
+		//float playerPosition = Player.rigidbody.position.y;
+		//float bombPosition = transform.position.y;
+		if (Player.gameObject.tag == "Player") {
+			
+			//bombPosition = playerPosition + 10.0f;
+			Debug.Log ("i'm hurt");
+		}
+
 	}
 }
