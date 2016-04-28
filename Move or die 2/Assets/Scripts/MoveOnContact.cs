24842dd0c7a7b8e0d4e45ff21835716b1cb4f11a
dorynguyen <knguyen@Kys-MacBook-Pro.local>
2016-04-27 20:07:29 -0400

    Explosion Made

--- Move or die 2/Assets/Scripts/MoveOnContact.cs
+++ Move or die 2/Assets/Scripts/MoveOnContact.cs
@@ -4,11 +4,10 @@ using System.Collections;
 public class MoveOnContact : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D Player) {
-		//float playerPosition = Player.rigidbody.position.y;
+		//float playerPosition = Player.rigidbody.position.y + 10f;
 		//float bombPosition = transform.position.y;
 		if (Player.gameObject.tag == "Player") {
-			
-			//bombPosition = playerPosition + 10.0f;
+			transform.position.y = Player.transform.position.y;
 			Debug.Log ("i'm hurt");
 		}
 
