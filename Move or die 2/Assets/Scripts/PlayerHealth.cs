1532cc68f8efe253aff51109be8d6c084f4a86ea
DungLe13 <dungle@bennington.edu>
2016-05-04 20:01:47 -0400

    Player Health script (almost there...)
    Need to Map the input values from 0 - 100 to output values from 0 - 1

---
+++ Move or die 2/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Vector2 previousPosition;
+    Vector2 currentPosition;
+    
+    public Image content;
+
+    PlayerMovement playerMovement;
+    bool isDead;
+    bool damaged;
+
+    void Awake()
+    {
+        playerMovement = GetComponent <PlayerMovement> ();
+  
+        currentHealth = startingHealth;
+    }
+
+    void Update()
+    {
+        //damaged = false;
+        currentPosition = GetComponent<Rigidbody2D>().position;
+        
+        TakeDamage();
+    }
+
+    public void TakeDamage()
+    {
+        // By not moving
+        if (currentPosition == previousPosition)
+        {
+            damaged = true;
+            currentHealth -= 1;
+            content.fillAmount = currentHealth;
+        } else
+        {
+            damaged = false;
+            currentHealth += 30;
+            content.fillAmount = currentHealth;
+
+            if (currentHealth >= 100)
+            {
+                currentHealth = 100;
+            }
+        }
+
+        previousPosition = currentPosition;
+
+        // By being hit by bombs
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        Destroy(this.gameObject);
+        playerMovement.enabled = false;
+    }
+}
