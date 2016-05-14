0e6e5a2ce175b0433f06bc00cd5fe2e9c99d3f64
dorynguyen <knguyen@Kys-MacBook-Pro.local>
2016-05-14 14:17:14 -0400

    Bomb Throwing direction
    Havent figured yet

--- Move or die 2/Assets/Scripts/PlayerMovement.cs
+++ Move or die 2/Assets/Scripts/PlayerMovement.cs
@@ -26,10 +26,11 @@ public class PlayerMovement : MonoBehaviour
 		throwButton = "space";
 	}
 
-	void Update(){
+	void Update ()
+	{
 		float currentY = GetComponent<Rigidbody2D> ().position.y;
 
-		if (Mathf.Abs(currentY - previousY)<0.001) {
+		if (Mathf.Abs (currentY - previousY) < 0.001) {
 			letsJump = true;
 
 		} else {
@@ -37,19 +38,23 @@ public class PlayerMovement : MonoBehaviour
 			//anim.SetBool("HaveBomb", false);
 
 		}
-		if ((Input.GetKeyDown ("up"))&&(letsJump==true)) {
+		if ((Input.GetKeyDown ("up")) && (letsJump == true)) {
 			player1Rigid.velocity = new Vector2 (player1Rigid.velocity.x, player1Rigid.position.y + jumpHeight);
 
 		}
-		previousY=currentY;
+		previousY = currentY;
 
-		if(Input.GetKeyDown("space")&&(loaded==true)){
+		//need to figure out how to change the bomb throwing direction
+		if (Input.GetKeyDown ("space") && (loaded == true)) {
+			if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+				Throw (right: false);
+			} else {
+				Throw (right: true);
 
-			Throw();
+			}
 		}
-
 	}
-		
+
 	void FixedUpdate ()
 	{
 		float horizontal_movement = Input.GetAxis ("Horizontal");
@@ -58,12 +63,12 @@ public class PlayerMovement : MonoBehaviour
 
 	}
 
-	void OnCollisionEnter2D(Collision2D other){
+	void OnCollisionEnter2D (Collision2D other)
+	{
 		//Debug.Log ("hey");
 
 		if ((other.gameObject.tag == "Bomb") && (loaded == false)) {
 			anim.SetBool ("HaveBomb", true);
-			Debug.Log ("hey");
 			Destroy (other.gameObject);
 			loaded = true;
 		
@@ -71,11 +76,29 @@ public class PlayerMovement : MonoBehaviour
 
 	}
 
-	private void Throw(){
+	private void Throw (bool right)
+	{
 		isThrown = true;
 		Rigidbody2D bombInstace = Instantiate (explosiveBomb, bombPosition.position, bombPosition.rotation) as Rigidbody2D;
+		/*
+		if(Input.GetKeyDown(KeyCode.LeftArrow)){
+			bombInstace.velocity = throwForce * bombPosition.right * -1;
+			Debug.Log (bombInstace.velocity);
+
+		}else{
 		bombInstace.velocity = throwForce * bombPosition.right;
-		Debug.Log (bombPosition.right);
+		}
+		*/
+		if (right == true) {
+			bombInstace.velocity = throwForce * bombPosition.right;
+			Debug.Log ("right");
+		} else  {
+			bombInstace.velocity = throwForce * bombPosition.right * -1;
+			Debug.Log ("left");
+
+		}
+
+
 		loaded = false;
 		anim.SetBool ("HaveBomb", false);
 	}
